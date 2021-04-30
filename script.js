b18b4ee502edcfdb93c7d3bbc84dd0f0ed5806f8
Amirka2 <70907990+Amirka2@users.noreply.github.com>
2021-05-01 02:39:57 +0500

    Add files via upload
    Отредактировано навМеню в адаптиве главной страницы. Добавлена кнопка ЛК в адаптиве главной страницы и еще мелкие косяки поправлены.

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+
+$('body').on('click', '.passwordCheckbox', function(){
+	if ($(this).is(':checked'))
+		{
+			$('#userPassword').attr('type', 'text');
+		} 
+	else 
+		{
+			$('#userPassword').attr('type', 'password');
+		}
+});
+
+function show_hide_password(target){
+	var input = document.getElementById('password-input');
+	if (input.getAttribute('type') == 'password') {
+		target.classList.add('view');
+		input.setAttribute('type', 'text');
+	} else {
+		target.classList.remove('view');
+		input.setAttribute('type', 'password');
+	}
+	return false;
+}
+
 let popup = document.getElementById("Eugene1"),
 	popupClose = document.getElementById("closeBtn"),
 	popupInfo = document.getElementById("popupInfo");
